9e705db53891f1a46975a8410f6ae02eb61c3e4d
Tokoni K <tokonix@gmail.com>
2024-10-16 14:31:05 +0100

    fix: add cli main file

--- src/genroutes/__init__.py
+++ src/genroutes/__init__.py
@@ -2,3 +2,4 @@
 __version__ = "0.0.1"
 
 from .generic_routes import Routes, HttpMethods, Service
+from .crud import to_json_non_recursive, to_json, filter_model, query_with_all_relationships, recursive_load
